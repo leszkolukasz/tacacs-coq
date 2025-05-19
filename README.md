a087cdd7e5bdf24748e7aa79f9d84d8fd419434f
Łukasz Leszko <leszko.lucas@gmail.com>
2025-05-19 23:13:08 +0200

    Add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+## TACACS protocol in Coq
+
+### Commands
+
+- `make` - Build the project
+- `make coq` - Build coq files
+- `make clean` - Clean the project
+- `make format` - Format the files
+
+### Notes
+
+- `coq` folder needs to be opened in separate VSCode window to handle imports correctly
--- src/servr/loop.ml
+++ src/servr/loop.ml
@@ -2,21 +2,19 @@ open Bindings
 open Types
 open Network
 open Unix
-
 open Protocol
 
-let loop_entry (state: unit): (unit, errorMsg) result =
+let loop_entry (state : unit) : (unit, errorMsg) result =
   let buffer_size = 1024 in
   let buffer = Bytes.make buffer_size '\000' in
   return ()
-;;
 
 let start_server (host : string) (port : int) : (unit, errorMsg) result =
   let socket = get_socket_connection host port in
   let he = gethostbyname host in
   let inet_addr = he.h_addr_list.(0) in
-  let addr = ADDR_INET(inet_addr, port) in
+  let addr = ADDR_INET (inet_addr, port) in
   bind socket addr;
-  let init_state = () in (* TODO popraw *)
-  loop_entry init_state;
-;;
+  let init_state = () in
+  (* TODO popraw *)
+  loop_entry init_state
